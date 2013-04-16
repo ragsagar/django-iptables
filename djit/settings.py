2db2829cdd0c9d1a6d9a05b21ad9559db0918fe8
Rag Sagar.V <ragsagar@gmail.com>
2013-04-17 01:13:37 +0530

    Adding apis to settings

--- djit/settings.py
+++ djit/settings.py
@@ -125,10 +125,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'apis',
 )
 
 # A sample logging configuration. The only tangible logging
